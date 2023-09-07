30877767dd376bc100f0b36443bce332e0a115c8
senr0w <special4student@gmail.com>
2023-09-07 12:04:31 +0700

    Добавьте файлы проекта.

---
+++ кодинг/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class program
+{
+    static void Main(string[] args)
+    {
+        double amount = 1;
+        double exchangeRate= 101.6;
+        string dollar = "USD";
+        string rubl = "RUB";
+        double convertamount = ConvertCurrency(amount, dollar, rubl, exchangeRate);
+        Console.WriteLine($"{amount} {dollar} равно {convertamount} {rubl}.");
+    }
+    static double ConvertCurrency(double amount, string dollar, string rubl, double exchangeRate)
+    {
+        if (dollar == rubl)
+        {
+            return amount;
+        }
+        double convertamount = amount * exchangeRate;
+        return convertamount;
+    }
+}
